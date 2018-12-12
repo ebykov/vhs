84dc4d82d70d78d5651cf8e5a4fdab8d8c230a97
evgeny <bykovzhukov@yandex.ru>
2018-12-12 17:48:08 +0300

    Updates

--- all.js
+++ all.js
@@ -1001,7 +1001,7 @@ var Special = function (_BaseSpecial) {
         this.container.classList.add('is-feed');
       }
 
-      Special.loadImages();
+      // Special.loadImages();
       Special.createElements();
 
       this.container.appendChild(EL.main);
